bf7e4df790e8f1eb46d8797946b83031d5994b29
ibraheem15 <ibraheemrehman15@gmail.com>
2022-11-01 19:26:24 +0500

    Created Grid

---
+++ Initial.py
@@ -0,0 +1,207 @@
+import pygame
+import random
+
+pygame.font.init()
+
+# Grid will be 10 x 20	
+
+# Global Variables
+screen_width = 800
+screen_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 20 height per block
+block_size = 30
+
+top_left_x = (screen_width - play_width) // 2
+top_left_y = screen_height - play_height
+
+
+# Shape Formats
+
+S = [['.....',
+      '......',
+      '..00..',
+      '.00...',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece(object):
+    # Constructor
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0  # number from 0-3
+    
+def create_grid(locked_positions={}): # Locked Positions is a dictionary which will have the position of the blocks that are already placed
+    grid = [[(0,0,0) for x in range(10)] for x in range(20)] # 2d list where each row (20) is a list of 10 values which are RGB values
+    
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if (col, row) in locked_positions:
+                c = locked_positions[(col, row)]
+                grid[row][col] = c
+    return grid
+
+def convert_shape_format(shape):
+    pass
+
+def valid_space(shape, grid):
+    pass
+
+def check_lost(positions):
+    pass
+
+def get_shape():	
+    return random.choice(shapes)
+
+def draw_text_middle(text, size, color, surface):  
+    pass
+   
+def draw_grid(surface,grid):
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            pygame.draw.rect(surface,
+                             grid[row][col],
+                             (top_left_x + col * block_size, top_left_y + row * block_size, block_size, block_size),
+                             1
+                            )
+    
+    pygame.draw.rect(surface,
+                     (255,0,0), # Red
+                     (top_left_x, top_left_y, play_width, play_height), 
+                     5 # Border width
+                    )
+    
+    
+
+def clear_rows(grid, locked):
+    pass
+
+def draw_next_shape(shape, surface):
+    pass
+
+def draw_window(surface, grid):
+    surface.fill((0,0,0)) # Fill the screen with black
+    
+    pygame.font.init()
+    font = pygame.font.SysFont('Corbel', 60)
+    label = font.render('Tetris', 1, (255,255,255)) # Render the text
+    
+    surface.blit(label, (top_left_x + play_width / 2 # Get the width of the text and divide it by 2 to get the middle
+                        - (label.get_width() / 2), 30
+                        )) # Blit the text to the screen
+    
+    draw_grid(surface, grid) # Draw the grid
+    pygame.display.update() # Update the display
+    
+
+def main():
+    pass
+
+def main_menu():
+    pass
+
+main_menu()  # start game     
